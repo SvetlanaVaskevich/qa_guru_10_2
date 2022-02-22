a83bf124f8e73f97795e5e07b58e9afd167ede6e
Svetlana Vaskevich <svaskevich@bk.ru>
2022-02-22 08:45:44 +0500

    OwnerTask added

--- src/test/java/bk/vaskevich/demoqa/OwnerTest.java
+++ src/test/java/bk/vaskevich/demoqa/OwnerTest.java
@@ -2,12 +2,14 @@ package bk.vaskevich.demoqa;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class OwnerTest {
 
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
+    @Tag("owner")
     @Test
     public void OwnerTests(){
         String login = config.login();
